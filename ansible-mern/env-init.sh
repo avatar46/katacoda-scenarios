cbdeef32430a5d44371720b41bf00ae819c9a63c
yilinchang <ch.yilin@icloud.com>
2022-04-14 20:30:01 +0200

    Modify env-init.sh

--- ansible-mern/env-init.sh
+++ ansible-mern/env-init.sh
@@ -1,9 +1,3 @@
 mkdir playbook
+cd playbook/ && touch mern.yml
 cd
-cd playbook/
-touch mern.yml
-cd
-apt update
-apt install software-properties-common
-add-apt-repository --yes --update ppa:ansible/ansible
-apt install ansible
